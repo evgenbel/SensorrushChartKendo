76e65deee21327bf76d56a72d563aa564c743fe8
pismenovd@gmail.com <Abra1234>
2016-01-06 11:43:44 +0300

    Initial commit

---
+++ wp-content/plugins/SensorrushChartKendo/chart.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Евгений on 05.01.2016.
+ */
+
+function refreshKendoChart(){
+    var chart = $("#chart").data("kendoChart");
+    chart.refresh();
+}
+
+function createChart() {
+    $("#chart").kendoChart({
+        dataSource: {
+            transport: {
+                read: {
+                    url: location.href,
+                    data: {ajaxurl: 1},
+                    dataType: "json"
+                }
+            },
+            sort: {
+                field: "year",
+                dir: "asc"
+            }
+        },
+        autoBind: true,
+        transitions: true,
+            title: {
+            text: "Spain electricity production (GWh)"
+        },
+        legend: {
+            position: "top"
+        },
+        seriesDefaults: {
+            type: "line"
+        },
+        series: [{
+            field: "Humidity",
+            name: "Humidity"
+        }],
+        categoryAxis: {
+            field: "ts",
+            labels: {
+                rotation: -90
+            },
+            crosshair: {
+                visible: true
+            }
+        },
+        valueAxis: {
+            labels: {
+                format: "N0"
+            },
+            majorUnit: 100
+        },
+        tooltip: {
+            visible: true,
+            shared: true,
+            format: "N0"
+        }
+    });
+
+    setInterval(refreshKendoChart, 20000);
+}
+
+
+
+
+$(document).ready(createChart);
+$(document).bind("kendo:skinChange", createChart);
